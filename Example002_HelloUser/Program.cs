06bfece6a7bef26516d7d66e38531a86ff7b8382
Vyacheslav <Vbaschorin@rambler.ru>
2022-05-08 21:03:25 +0500

    Example002_HelloUser

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.Write ("Введите Ваше имя");
-string username = Console.ReadLine () ;
+string username = Console.ReadLine ();
 Console.Write ("Привет, ");
-Console.Write (username);
+Console.Write (username); 
+
