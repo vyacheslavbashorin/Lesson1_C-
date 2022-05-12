57da4ffc2cdc0634c306edb2e22f82f3d56724c0
Vyacheslav <Vbaschorin@rambler.ru>
2022-05-12 21:46:18 +0500

    Example012

---
+++ Example012/Program.cs
@@ -0,0 +1,29 @@
+﻿int A = 2;
+int B = -3;
+int C = -7;
+
+if (A > 0)
+{
+    Console.WriteLine (A);
+}
+else 
+{
+   Console.WriteLine (-A); 
+}
+
+if (B > 0)
+{
+    Console.WriteLine (B);
+}
+else 
+{
+   Console.WriteLine (-B); 
+}
+if (C > 0)
+{
+    Console.WriteLine (C);
+}
+else 
+{
+   Console.WriteLine (-C); 
+}
