d91aaedaa9e055675d29211d85691495d496ec79
Vyacheslav <VBaschorin@rambler.ru>
2022-07-02 13:42:34 +0500

    Example037

---
+++ Example037/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int Number = new Random().Next(100, 1000); 
+int NewNumber = 0; 
+
+ 
+int NumberWithoutSecDig() 
+{ 
+    NewNumber = Number / 100 * 10 + Number % 10; 
+    return NewNumber; 
+} 
+ 
+NumberWithoutSecDig(); 
+ 
+ 
+Console.WriteLine(); 
+Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}"); 
+Console.WriteLine(); 
+
