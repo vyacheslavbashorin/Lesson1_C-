9d450ac96e9f5994a5715c972bfaf38d09f2d912
Vyacheslav <VBaschorin@rambler.ru>
2022-07-02 13:24:37 +0500

    Example036

---
+++ Example036/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write ("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32 (Console.ReadLine());
+string stringNumber = Convert.ToString (threeDigitNumber);
+Console.WriteLine("Вторая цифра этого числа -> "+stringNumber[1]);
