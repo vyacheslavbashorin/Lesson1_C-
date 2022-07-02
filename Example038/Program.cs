efcda1227580160ad7ff4f5b25f11d6fcf3bfec8
Vyacheslav <VBaschorin@rambler.ru>
2022-07-02 13:50:39 +0500

    Example038

---
+++ Example038/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else 
+{
+  Console.WriteLine("-> третьей цифры нет");
+}
