b3d6202e545bb837b0735cdf941d178043c37db7
Vyacheslav <VBaschorin@rambler.ru>
2022-07-09 14:38:00 +0500

    Example062

---
+++ Example062/Program.cs
@@ -0,0 +1,20 @@
+﻿//Ввод числа с приглашением
+int Prompt (string message)
+{
+    Console.Write (message);
+    return int.Parse (Console.ReadLine ());
+}
+//Вычисление суммы цифр числа
+int CalcSumdigits (int number)
+{
+    int sum = 0;
+    while (number >0)
+    {
+    sum+=number%10;
+    number=number/10;
+    }
+    return sum;
+}
+int number= Prompt ("Введите число: ");
+int result = CalcSumdigits (number);
+System.Console.WriteLine($"Сумма цифр равна {result}");  
