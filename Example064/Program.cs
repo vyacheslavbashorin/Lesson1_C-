4de4a385c82c901cd1ef7931ffb900f88a8f99b6
Vyacheslav <VBaschorin@rambler.ru>
2022-07-09 14:41:59 +0500

    Example064

---
+++ Example064/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Оформите программу ввиде функций (пригодится в дальнейшем)
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
